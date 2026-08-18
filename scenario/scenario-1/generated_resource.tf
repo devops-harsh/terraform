df60bdd281e9c2f4ba31b296edb5f33446159a4e
HarshLKadam <devharsh2409@gmail.com>
2026-08-18 12:07:34 +0530

    migration of existing recource and drift detection

---
+++ scenario/scenario-1/generated_resource.tf
@@ -0,0 +1,29 @@
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by Terraform
+# resource "aws_instance" "import_instance" {
+#   ami                                  = "ami-01a00762f46d584a1"
+#   associate_public_ip_address          = true
+#   availability_zone                    = "ap-south-1b"
+#   disable_api_stop                     = false
+#   disable_api_termination              = false
+#   ebs_optimized                        = false
+#   force_destroy                        = false
+#   get_password_data                    = false
+#   hibernation                          = false
+#   instance_initiated_shutdown_behavior = "stop"
+#    ...........
+#    ...........
+#    ...........
+#   root_block_device {
+#     delete_on_termination = true
+#     encrypted             = false
+#     iops                  = 3000
+#     tags                  = {}
+#     tags_all              = {}
+#     throughput            = 125
+#     volume_size           = 8
+#     volume_type           = "gp3"
+#   }
+# }
---
+++ scenario/scenario-1/main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# import {
+#     id = "resource_id"
+
+#     to = aws_instance.import_instance
+# }
+
+resource "aws_instance" "import_instance" {
+  ami = "ami-01a00762f46d584a1"
+  #   associate_public_ip_address          = true
+  availability_zone                    = "ap-south-1b"
+  disable_api_stop                     = false
+  disable_api_termination              = false
+  ebs_optimized                        = false
+  force_destroy                        = false
+  get_password_data                    = false
+  hibernation                          = false
+  instance_initiated_shutdown_behavior = "stop"
+  instance_type                        = "t2.micro"
+  key_name                             = "terraform_new"
+  
+
+#    ...........
+#    ...........
+#    ...........
+
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    iops                  = 3000
+    tags                  = {}
+    tags_all              = {}
+    throughput            = 125
+    volume_size           = 8
+    volume_type           = "gp3"
+  }
+}
